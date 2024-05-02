b7f5f599d824e649420251d790bdc12e3b483c31
ManeeshaNetthikoppula <netthikoppulamaneesha@gmail.com>
2024-05-02 09:25:53 +0530

    maneesha is changed

---
+++ even or odd num.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")();
+
+const num = parseInt(prompt("enter the number:"));
+if(num%2===0){
+    console.log(num+" is even number");
+} else {
+    console.log(num+"is odd number");
+}
---
+++ multable.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+
+const n = parseInt(prompt("enter the n:"));
+
+for (let i=1; i<11: i++){
+    console.log(n +" * "+i+" = "+(n*i));
+} 
